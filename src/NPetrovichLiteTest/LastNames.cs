92bd24e5a0d4a52687afcdbbfc64336806ee5f40
mikhail.barg@gmail.com <mikhail.barg@gmail.com>
2016-07-03 23:19:34 +0300

    Generally inflection is working!

---
+++ src/NPetrovichLiteTest/LastNames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPetrovichLite;
+
+namespace NPetrovichLiteTest
+{
+    [TestClass]
+    public class LastNames
+    {
+        private Petrovich petrovich;
+
+        [TestInitialize]
+        public void Init()
+        {
+            petrovich = new Petrovich();
+        }
+
+        [TestMethod]
+        public void Test01()
+        {
+            Assert.AreEqual("Иванов", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Nominative));
+        }
+        [TestMethod]
+        public void Test02()
+        {
+            Assert.AreEqual("Иванова", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Genitive));
+        }
+        [TestMethod]
+        public void Test03()
+        {
+            Assert.AreEqual("Иванову", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Dative));
+        }
+        [TestMethod]
+        public void Test04()
+        {
+            Assert.AreEqual("Иванова", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Accusative));
+        }
+        [TestMethod]
+        public void Test05()
+        {
+            Assert.AreEqual("Ивановым", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Instrumental));
+        }
+        [TestMethod]
+        public void Test06()
+        {
+            Assert.AreEqual("Иванове", petrovich.InflectNamePart("Иванов", NamePart.LastName, Gender.Male, Case.Prepositional));
+        }
+        
+    }
+}
