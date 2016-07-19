9a8b47a69c2b65ad28cea29fe8a85c5a1880ca74
mikhail-barg <mikhail.barg@gmail.com>
2016-07-19 10:57:16 +0300

    Working on exporting opencorpora: * added saving wit unix linebreaks, no BOM * working surnames and firstnames

--- src/OpenCorporaParser/Program.cs
+++ src/OpenCorporaParser/Program.cs
@@ -4,31 +4,79 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.IO.Compression;
 
 namespace OpenCorporaParser
 {
     class Program
     {
+        private const string dictUrl = @"http://opencorpora.org/files/export/dict/dict.opcorpora.xml.zip";
+        private const string dictArcFile = @"dict.opcorpora.xml.zip";
+        private const string dictFile = @"dict.opcorpora.xml";
+
         static void Main(string[] args)
         {
-            string filename = args[0];
-            Console.WriteLine("reading dom " + filename);
+
+            Console.WriteLine($"Downloading {dictArcFile} from OpenCorpora");
+            if (!File.Exists(dictArcFile))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(dictUrl, dictArcFile);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Already exists");
+            }
+
+            Console.WriteLine($"Unpacking to {dictFile}");
+            if (!File.Exists(dictFile))
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(dictArcFile))
+                {
+                    archive.Entries.Where(item => item.FullName == dictFile).First().ExtractToFile(dictFile);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Already exists");
+            }
+
+            Console.WriteLine($"reading dom from {dictFile}");
             XmlDocument doc = new XmlDocument();
-            using (StreamReader reader = new StreamReader(filename, Encoding.UTF8))
+            using (StreamReader reader = new StreamReader(dictFile, Encoding.UTF8))
             {
                 doc.Load(reader);
             }
+            ProcessNamePart(doc, "surnames", "Surn");
+            ProcessNamePart(doc, "firstnames", "Name");
+            ProcessNamePart(doc, "midnames", "Patr");
 
-            Console.WriteLine("listing surnames");
-            XmlNodeList nodes = doc.SelectNodes("/dictionary/lemmata/lemma[l/g[@v=\"Surn\"] and f/g[@v=\"sing\"]]");
+        }
+
+        private static void ProcessNamePart(XmlDocument doc, string namePart, string namePartGrammeme)
+        {
+            Console.WriteLine($"listing {namePart}");
+            XmlNodeList nodes = doc.SelectNodes(
+                $@"/dictionary/lemmata/lemma[
+                        l/g[@v=""{namePartGrammeme}""] 
+                        and f/g[@v=""sing""] 
+                        and not(l/g[@v=""Erro""]) 
+                        and not(l/g[@v=""Abbr""]) 
+                        and not(l/g[@v=""Infr""])
+                ]"
+            );
 
             OrderedDictionary lemmaToGender = new OrderedDictionary();
             Console.WriteLine("writing to file");
-            using (StreamWriter writerFull = new StreamWriter("surnames.oc.tsv", false, Encoding.UTF8))
+            using (StreamWriter writerFull = new StreamWriter($"{namePart}.oc.tsv", false, new UTF8Encoding(false)))
             {
+                writerFull.NewLine = "\n";
                 writerFull.WriteLine("lemma\tword\tgrammemes");
                 foreach (XmlNode lemmaNode in nodes)
                 {
@@ -36,14 +84,18 @@ namespace OpenCorporaParser
                     string lemma = lemmaSubNode.Attributes["t"].Value.ToUpper();
                     List<string> grammemes = lemmaSubNode.OfType<XmlNode>().Select(g => g.Attributes["v"].Value).ToList();
                     string gender = null;
-                    bool isFixed = false;
+                    //bool isFixed = false;
                     foreach (string gr in grammemes)
                     {
+                        if (gr == namePartGrammeme)
+                        {
+                            continue;   //already know it;
+                        }
+
                         switch (gr)
                         {
                         case "NOUN":    //существительное
                         case "anim":    //одушевленное
-                        case "Surn":    //фамилия
                         case "Sgtm":    //единственное число (singularia tantum)
                                         //not interesting, or already known, nothing to do
                             break;
@@ -64,7 +116,6 @@ namespace OpenCorporaParser
                             }
                             gender = "жр";
                             break;
-                        case "Ms-f":    //общий род
                         case "GNdr":    //род / род не выражен
                             if (gender != null && gender != "мр-жр")
                             {
@@ -72,8 +123,17 @@ namespace OpenCorporaParser
                             }
                             gender = "мр-жр";
                             break;
+                        case "Ms-f":    //общий род
+                            if (gender == null)
+                            {
+                                throw new ApplicationException($"Two genders for lemma {lemma}");
+                            }
+                            //gender = "мр-жр";
+                            break;
                         case "Fixd":
-                            isFixed = true;
+                            //isFixed = true; //for fixed 
+                            break;
+                        case "inan":    //strangely happens sometimes, eg. "РОЖЕ"
                             break;
                         default:
                             Console.WriteLine(gr);
@@ -89,37 +149,55 @@ namespace OpenCorporaParser
                         //already had an item with same lemma, so it must be another gender
                         if ((string)lemmaToGender[lemma] == gender)
                         {
-                            throw new ApplicationException($"Duplicate lemma {lemma} with same gender {gender}");
+                            //for some reason happens with "АБДАЛОВИЧ"
+                            //throw new ApplicationException($"Duplicate lemma {lemma} with same gender {gender}");
                         }
-                        lemmaToGender[lemma] = "мр-жр";
+                        else
+                        {
+                            lemmaToGender[lemma] = "мр-жр";
+                        }
                     }
                     else
                     {
                         lemmaToGender[lemma] = gender;
                     }
 
+                    /*
                     if (isFixed)
                     {
                         writerFull.WriteLine($"{lemma}\t{lemma}\t{gender},ед,0");
                     }
                     else
                     {
-                        foreach (XmlNode childNode in lemmaNode.SelectNodes("f[g[@v=\"sing\"]]"))
+                    */
+                    foreach (XmlNode childNode in lemmaNode.SelectNodes(
+                        @"f[
+                                g[@v=""sing""] 
+                                and not(g[@v=""V-ey""]) and not(g[@v=""V-oy""])
+                                and not(g[@v=""Dist""]) and not(g[@v=""Erro""])
+                                and not(g[@v=""voct""]) and not(g[@v=""gen1""]) and not(g[@v=""gen2""]) and not(g[@v=""acc2""]) and not(g[@v=""loc1""]) and not(g[@v=""loc2""])
+                        ]"
+                        ))
+                    {
+                        string form = childNode.Attributes["t"].Value.ToUpper();
+                        string @case = ConvertCase(childNode, lemma);
+                        if (@case == null)
                         {
-                            string form = childNode.Attributes["t"].Value.ToUpper();
-                            string @case = ConvertCase(childNode, lemma);
-                            if (@case == null)
-                            {
-                                continue;   //unsupported weird case
-                            }
-                            writerFull.WriteLine($"{lemma}\t{form}\t{gender},ед,{@case}");
+                            continue;   //unsupported weird case
                         }
+                        if (childNode.ChildNodes.Count != 2)
+                        {
+                            throw new ApplicationException($"some strange info for lemma {lemma} in form {form}");
+                        }
+                        writerFull.WriteLine($"{lemma}\t{form}\t{gender},ед,{@case}");
                     }
+                    //}
                 }
             }
 
-            using (StreamWriter writerGender = new StreamWriter("surnames.oc.gender.tsv", false, Encoding.UTF8))
+            using (StreamWriter writerGender = new StreamWriter($"{namePart}.oc.gender.tsv", false, new UTF8Encoding(false)))
             {
+                writerGender.NewLine = "\n";
                 writerGender.WriteLine("lemma\tgender");
                 foreach (DictionaryEntry entry in lemmaToGender)
                 {
@@ -128,7 +206,7 @@ namespace OpenCorporaParser
             }
         }
 
-private static string ConvertCase(XmlNode childNode, string lemma)
+        private static string ConvertCase(XmlNode childNode, string lemma)
         {
             string @case = null;
             string source = childNode.SelectSingleNode("g[@v!=\"sing\"]").Attributes["v"].Value;
